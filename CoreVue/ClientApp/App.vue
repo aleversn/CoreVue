9ec3ab092f398c84237a09c626229635201e6f67
alever_sn <759636538@qq.com>
2019-01-23 11:54:44 +0800

    添加项目文件。

---
+++ CoreVue/ClientApp/App.vue
@@ -0,0 +1,10 @@
+﻿<template>
+    <div>
+        <h1>Hello World!</h1>
+        <h1>.Net Core & Vue</h1>
+    </div>
+</template>
+
+<script>
+export default { }
+</script>
---
+++ CoreVue/ClientApp/index.js
@@ -0,0 +1,25 @@
+﻿import Vue from 'vue';
+import VueRouter from 'vue-router';
+import App from './App.vue';
+
+
+Vue.config.productionTip = false;
+Vue.use(VueRouter);
+
+const routes = [
+    {
+        path: '/',
+        component: App
+    }
+];
+
+const router = new VueRouter({
+    routes,
+    mode: 'history'
+});
+
+new Vue({
+    el: '#app',
+    render: h => h(App),
+    router
+});
